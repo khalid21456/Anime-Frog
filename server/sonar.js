f552f6e638d06a47ca4c42c696ca616c4eba76aa
khalid21456 <khalidedaoudi8@gmail.com>
2025-09-05 22:52:27 +0100

    sonar

--- server/sonar.js
+++ server/sonar.js
@@ -1,6 +1,8 @@
-const scanner = require('sonarqube-scanner');
-
+const scanner = require('sonarqube-scanner').default || require('sonarqube-scanner');
 scanner({
-  serverUrl: 'http://sonarqube:9000',
+  serverUrl: 'http://localhost:9000',
   token: 'squ_80f6c8176d559996c13e0553f46a659acfaa2b9c',
+  options: {
+
+  }
 }, () => {});
