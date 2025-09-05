9ed17836cdaefdbc1c1d777a266a996810021614
KHALID EDAOUDI <143753740+khalid21456@users.noreply.github.com>
2025-09-05 22:56:25 +0100

    Update sonar.js

--- server/sonar.js
+++ server/sonar.js
@@ -1,6 +1,6 @@
 const scanner = require('sonarqube-scanner').default || require('sonarqube-scanner');
 scanner({
-  serverUrl: 'http://localhost:9000',
+  serverUrl: 'http://sonarqube:9000',
   token: 'squ_80f6c8176d559996c13e0553f46a659acfaa2b9c',
   options: {
 
